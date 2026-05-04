0e4672237d944546d15ce06f226aa3ae908e2d12
jaustrylle <sanren.reese@gmail.com>
2026-05-04 18:47:22 -0500

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,1 +1,27 @@
 # Forward chaining inference engine
+
+def forward_chain(facts, rules):
+
+    actions = []
+    explanations = []
+
+    changed = True
+
+    while changed:
+        changed = False
+
+        for rule in rules:
+
+            if rule.condition(facts):
+
+                if rule.action not in actions:
+                    actions.append(rule.action)
+                    explanations.append({
+                        "action": rule.action,
+                        "rule": rule.name,
+                        "why": rule.explanation
+                    })
+
+                    changed = True
+
+    return actions, explanations
