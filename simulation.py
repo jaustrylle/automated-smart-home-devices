d62690cbda2be04e343ad43ceb8f33dc7a88d06c
jaustrylle <sanren.reese@gmail.com>
2026-05-04 18:48:53 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,1 +1,27 @@
 # Dataset and environment simulation
+
+import pandas as pd
+
+def load_energy_data(path):
+
+    df = pd.read_csv(path)
+
+    df["Energy Consumption (kWh)"] *= 100
+
+    return df
+
+
+def simulate_time_series(df):
+
+    # Convert dataset into pseudo-events
+    events = []
+
+    for _, row in df.head(50).iterrows():
+
+        events.append({
+            "time": int(row["Unix Timestamp"] % 24),
+            "motion": row["Appliance Usage"] == 1,
+            "temperature": row["Voltage"]
+        })
+
+    return events
