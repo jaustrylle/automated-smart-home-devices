0b572ba7a2efb61b214611f725c5104c3a1749f0
jaustrylle <sanren.reese@gmail.com>
2026-05-04 18:47:41 -0500

    Update explain.py

--- explain.py
+++ explain.py
@@ -1,1 +1,14 @@
 # Explainability layer
+
+def format_explanations(explanations):
+
+    output = []
+
+    for e in explanations:
+        output.append(
+            f"Action: {e['action']}\n"
+            f"Triggered by rule: {e['rule']}\n"
+            f"Reason: {e['why']}\n"
+        )
+
+    return output
