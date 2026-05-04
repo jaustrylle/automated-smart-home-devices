902c66a5f8846a66af1345751681caaccb7c6e80
jaustrylle <sanren.reese@gmail.com>
2026-05-04 18:46:17 -0500

    Update facts.py

--- facts.py
+++ facts.py
@@ -1,1 +1,16 @@
 # Sensor state (facts)
+
+class Facts:
+    def __init__(self):
+        self.data = {
+            "motion_kitchen": False,
+            "time": 21,
+            "temperature": 72,
+            "override": False
+        }
+
+    def update(self, key, value):
+        self.data[key] = value
+
+    def get(self):
+        return self.data
